278bd4a6ac0c11e6a2f541f1e1f4a4d3b25b041b
isaquias1 <xboxisaquias@gmail.com>
2025-05-13 14:59:14 -0300

    Primeiro commit

---
+++ Entities/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    /// <summary>
+    /// Classe Aluno e suas propiedades
+    /// </summary>
+    public class Aluno
+    {
+        public Guid id { get; set; }
+        public string? Nome { get; set; }
+        public string? Escola { get; set; }
+        public string? Professor { get; set; }
+    }
+
+}
